497da0702422f245196715f023c026aaf23828d7
Harry Dodson <harry.lee.dodson@gmail.com>
2024-04-09 09:30:59 -0400

    added getters/setters

--- src/main/java/com/dodson/backendcoderockr/domain/model/UserModel.java
+++ src/main/java/com/dodson/backendcoderockr/domain/model/UserModel.java
@@ -20,4 +20,24 @@ public class UserModel {
 	
 	@Column(name = "last_name", columnDefinition = "varchar(25)", nullable = false)
 	private String lastName;
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 }
