ae2be613710538fe8b6c7b22d0b351d5bb60b8a9
Harry Dodson <harry.lee.dodson@gmail.com>
2024-04-10 09:31:49 -0400

    adding investment model. updated user model for their investments.

--- src/main/java/com/dodson/backendcoderockr/domain/model/UserModel.java
+++ src/main/java/com/dodson/backendcoderockr/domain/model/UserModel.java
@@ -1,10 +1,14 @@
 package com.dodson.backendcoderockr.domain.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -20,6 +24,9 @@ public class UserModel {
 	
 	@Column(name = "last_name", columnDefinition = "varchar(25)", nullable = false)
 	private String lastName;
+
+	@OneToMany(mappedBy = "parent")
+	private List<InvestmentModel> investments;
 
 	public Long getId() {
 		return id;
@@ -39,5 +46,17 @@ public class UserModel {
 
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+
+	public List<InvestmentModel> getInvestments() {
+		return investments;
+	}
+
+	public void addInvestment(InvestmentModel investment) {
+		if (investments == null) {
+			investments = new ArrayList<>();
+		}
+		investments.add(investment);
+		investment.setParent(this);
 	}
 }
