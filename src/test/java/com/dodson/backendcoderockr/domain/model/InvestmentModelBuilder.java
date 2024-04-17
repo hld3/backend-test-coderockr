b7549341a2b52794100bb7c4cd17bc441a80686d
Harry Dodson <harry.lee.dodson@gmail.com>
2024-04-17 09:18:18 -0400

    added InvestmentModelBuilder.

---
+++ src/test/java/com/dodson/backendcoderockr/domain/model/InvestmentModelBuilder.java
@@ -0,0 +1,18 @@
+package com.dodson.backendcoderockr.domain.model;
+
+import com.github.javafaker.Faker;
+
+public class InvestmentModelBuilder {
+
+	Faker faker = new Faker();
+
+	private double amount = faker.number().randomDouble(2, 1, 5);
+	private long creationDate = faker.number().randomNumber(13, true);
+
+	public InvestmentModel build() {
+		InvestmentModel im = new InvestmentModel();
+		im.setAmount(amount);
+		im.setCreationDate(creationDate);
+		return im;
+	}
+}
