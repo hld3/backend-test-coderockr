b13ccff3085d0b72810d204913c43ed7bd7d1884
Harry Dodson <harry.lee.dodson@gmail.com>
2024-04-25 15:10:43 -0400

    final checkstyle updates.

--- src/main/java/com/dodson/backendcoderockr/repository/UserRepository.java
+++ src/main/java/com/dodson/backendcoderockr/repository/UserRepository.java
@@ -10,19 +10,19 @@ import com.dodson.backendcoderockr.domain.model.UserModel;
 
 public interface UserRepository extends JpaRepository<UserModel, Long> {
 
-	/**
-	 * Testing @Query.
-	 * Retrieve the user with the given UUID.
-	 * @param userId The userId to search for.
-	 * @return The {@link UserModel} or null if not found.
-	 */
-	@Query("SELECT u FROM UserModel u WHERE userId = :userId")
-	UserModel findUserByUserId(@Param("userId") UUID userId);
+        /**
+         * Testing @Query.
+         * Retrieve the user with the given UUID.
+         * @param userId The userId to search for.
+         * @return The {@link UserModel} or null if not found.
+         */
+        @Query("SELECT u FROM UserModel u WHERE userId = :userId")
+        UserModel findUserByUserId(@Param("userId") UUID userId);
 
-	/**
-	 * Retrieve the user with the given UUID.
-	 * @param userId The userId to search for.
-	 * @return The {@link UserModel} or null if not found.
-	 */
-	UserModel findByUserId(UUID userId);
+        /**
+         * Retrieve the user with the given UUID.
+         * @param userId The userId to search for.
+         * @return The {@link UserModel} or null if not found.
+         */
+        UserModel findByUserId(UUID userId);
 }
