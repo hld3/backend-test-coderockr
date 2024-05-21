2d4cf3439e70d15b22dde5d2aa8c38441f043006
Harry Dodson <harry.lee.dodson@gmail.com>
2024-05-21 14:35:41 -0400

    formatting.

--- src/main/java/com/dodson/backendcoderockr/repository/UserRepository.kt
+++ src/main/java/com/dodson/backendcoderockr/repository/UserRepository.kt
@@ -4,12 +4,11 @@ import com.dodson.backendcoderockr.domain.model.UserModel
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.UUID
 
-interface UserRepository: JpaRepository<UserModel, Long> {
-
-        /**
-         * Retrieve the user with the given UUID.
-         * @param userId The userId to search for.
-         * @return The [UserModel] or null if not found.
-         */
-        fun findByUserId(userId: UUID?): UserModel?
+interface UserRepository : JpaRepository<UserModel, Long> {
+    /**
+     * Retrieve the user with the given UUID.
+     * @param userId The userId to search for.
+     * @return The [UserModel] or null if not found.
+     */
+    fun findByUserId(userId: UUID?): UserModel?
 }
